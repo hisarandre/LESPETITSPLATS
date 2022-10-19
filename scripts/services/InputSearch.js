ddcdc5669a8cece0d39bff5f5feee7c39273f600
Sara Andre <hisarandre@gmail.com>
2022-10-20 01:59:54 +0900

    Add branch searchFilter v1

--- scripts/services/InputSearch.js
+++ scripts/services/InputSearch.js
@@ -10,15 +10,14 @@ export class InputSearch {
 
   // get input entry, filter data and return in the constructor array
   filter(entry) {
-    this.recipesFromInput = [];
+    let filteredRecipe = this.recipeData.filter(
+      (recipe) =>
+        recipe._name.toLowerCase().includes(entry.toLowerCase()) ||
+        recipe._description.toLowerCase().includes(entry.toLowerCase()) ||
+        recipe._ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(entry.toLowerCase()))
+    );
 
-    for (let i = 0; i < this.recipeData.length; i++) {
-      if (
-        this.recipeData[i]._name.toLowerCase().includes(entry.toLowerCase()) ||
-        this.recipeData[i]._description.toLowerCase().includes(entry.toLowerCase()) ||
-        this.recipeData[i]._ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(entry.toLowerCase()))
-      )
-        this.recipesFromInput.push(this.recipeData[i]);
-    }
+    this.recipesFromInput = [];
+    filteredRecipe.map((recipe) => this.recipesFromInput.push(recipe));
   }
 }
